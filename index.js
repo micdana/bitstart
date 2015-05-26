094c4c3ca6d130b30f8cd54247b2de21e6bc13a0
micdana <1020.michael@gmail.com>
2015-05-26 21:51:35 +0000

    one more time

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// var express = require('express');
-// var fs = require('fs');
+var express = require('express');
+var app = express.createServer(express.logger());
 
-// var data = fs.readFileSync("webindex.html", "utf-8");
+app.get('/', function(request, response){
+   response.send('Hello World');
+});
 
-// var app = express.createServer(express.logger());
-
-// app.get('/', function(request, response){
-//  response.send(data);
-// });
-
-//  var port = process.env.PORT || 5000;
-// app.listen(post, function() {
-//  console.log("listening on " + port);
-// });
+var port = process.env.PORT || 5000;
+app.listen(post, function() {
+   console.log("listening on " + port);
+});
